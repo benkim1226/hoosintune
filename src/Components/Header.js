33ea7884208a588904003b45d0bb7b34cbaf3a60
Ben Kim <dan4zy@virginia.edu>
2022-04-24 16:38:24 -0400

    hello

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Styles/Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Hoos In Tune</h1>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/Components/Subheader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Styles/Subheader.css';
+
+const Subheader = () => {
+    return (
+        <div>
+            <h1>A program that recommends music to UVA students based on Charlottesville weather</h1>
+        </div>
+    );
+}
+
+export default Subheader;
